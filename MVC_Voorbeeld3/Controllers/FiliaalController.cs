230e299c9aff06b597658b68b95dd3f83abd0d9b
Ben Vanhoutvinck <vanhoutvinckben@gmail.com>
2016-07-08 10:54:57 +0200

    first commit

---
+++ MVC_Voorbeeld3/Controllers/FiliaalController.cs
@@ -0,0 +1,43 @@
+﻿using MVC_Voorbeeld3.Models;
+using MVC_Voorbeeld3.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld3.Controllers
+{
+    public class FiliaalController : Controller
+    {
+        private FiliaalService filiaalService = new FiliaalService();
+        private HoofdZetelService hoofdZetelService = new HoofdZetelService();
+        // GET: Filiaal
+        public ActionResult Index()
+        {
+            var hoofdZetel = hoofdZetelService.Read();
+            ViewBag.hoofdZetel = hoofdZetel;
+            var filialen = filiaalService.FindAll();
+            return View(filialen);
+        }
+        public ActionResult Verwijderen(int id)
+        {
+            var filiaal = filiaalService.Read(id);
+            return View(filiaal);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var filiaal = filiaalService.Read(id);
+            this.TempData["filiaal"] = filiaal;
+            filiaalService.Delete(id);
+            return RedirectToAction("Verwijderd");
+        }
+
+        public ActionResult Verwijderd()
+        {
+            var filiaal = (Filiaal)this.TempData["filiaal"];
+            return View(filiaal);
+        }
+    }
+}
---
+++ MVC_Voorbeeld3/Controllers/PersoonController.cs
@@ -0,0 +1,23 @@
+﻿using MVC_Voorbeeld3.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld3.Controllers
+{
+    public class PersoonController : Controller
+    {
+        private PersoonService persoonService = new PersoonService();
+        public ActionResult Index()
+        {
+            return View(persoonService.FindAll());
+        }
+        [HttpGet]
+        public ActionResult VerwijderForm(int id)
+        {
+            return View(persoonService.FindByID(id));
+        }
+    }
+}
